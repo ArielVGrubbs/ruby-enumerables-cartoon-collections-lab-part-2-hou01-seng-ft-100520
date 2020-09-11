1a1ec7a2b2e1441b508a0d96c1311505857433ae
Ariel Grubbs <arielvictor1234@gmail.com>
2020-09-11 01:10:54 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -24,11 +24,9 @@ end
 
 def find_valid_calls(planeteer_calls)
   valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
-  first_valid_call = nil
   planeteer_calls.find do |calls|
     valid_calls.include? calls
   end
-  first_valid_call
 end
 
 
